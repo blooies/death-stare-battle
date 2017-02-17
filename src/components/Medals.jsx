532d6cc24c767190e035e4ccc708d2074335f4ed
blooies <joy28@msn.com>
2017-02-17 15:55:26 -0500

    render how many medals you want on the homepage

---
+++ src/components/Medals.jsx
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import Medal from './Medal.jsx';
+
+class Medals extends Component {
+    getMedals() {
+        let medalTypes = ['gold', 'silver', 'bronze'];
+        let medals = [];
+        let j = 0;
+        for (var i=0; i<=this.props.numberOfMedals; i++) {
+            let medal = <Medal 
+                type={medalTypes[j]}
+                key={i}
+            />
+            medals.push(medal);
+            j++;
+            if (j === medalTypes.length) j = 0;
+        }
+        return medals;
+    }
+
+    render() {
+        return (
+            <div className='container'>
+                {this.getMedals()}
+            </div>
+        )
+    }
+}
+
+export default Medals;
