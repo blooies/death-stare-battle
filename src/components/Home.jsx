36ee312f11e0cea0aa84aebdcea5e46a1e43977f
blooies <joy28@msn.com>
2017-02-18 17:32:00 -0500

    updated home component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Medals from './Medals.jsx';
+import Uploader from './Uploader.jsx';
 
 class Home extends Component {
   render() {
@@ -10,9 +11,7 @@ class Home extends Component {
         </div>
         <Medals numberOfMedals={40}></Medals>
         <h2 className='action'>give me your best #phelpsface</h2>
-        <div className='camera'>
-          <input id='uploadImage' type='file' accept='image/*;capture=camera'/>
-        </div>
+        <Uploader/>
       </div>
     );
   }
